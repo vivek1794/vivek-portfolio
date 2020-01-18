490d5c683929092ff3b1bba7aa4947cc86581739
Vivek Chanddru <vivek.chanddru@gmail.com>
2020-01-18 14:04:47 -0800

    Updates

---
+++ precache-manifest.7d50b33e796ff5e8948bd08dfc6054d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfceb6ef39455e849d7336c582ccf4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687905d6faaac8ef2e76",
+    "url": "/static/js/2.a9098fbd.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.a9098fbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7f02e256c16e4268db5",
+    "url": "/static/js/main.04d20237.chunk.js"
+  },
+  {
+    "revision": "d8f3716c897e7679661c",
+    "url": "/static/js/runtime-main.7ae16df5.js"
+  },
+  {
+    "revision": "00c2e98bca634a483ffc00bd95d4a2d2",
+    "url": "/static/media/avatar.00c2e98b.jpg"
+  }
+]);
