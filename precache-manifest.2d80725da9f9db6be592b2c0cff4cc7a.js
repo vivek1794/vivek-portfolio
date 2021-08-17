f407ebb6ac13e992ac16ebd88ac984e896f8702c
Vivek Chanddru <vivek.chanddru@gmail.com>
2021-08-16 19:40:29 -0700

    Updates

---
+++ precache-manifest.2d80725da9f9db6be592b2c0cff4cc7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb00733b55f1821459d2d941c506a104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46740304672c00a8dab",
+    "url": "/static/js/2.6f241f71.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.6f241f71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "773249be3a94ccafdd0c",
+    "url": "/static/js/main.f742e90c.chunk.js"
+  },
+  {
+    "revision": "d8f3716c897e7679661c",
+    "url": "/static/js/runtime-main.7ae16df5.js"
+  },
+  {
+    "revision": "cc768dc6ae0c05ce1853bd4c04a16c97",
+    "url": "/static/media/avatar.cc768dc6.jpg"
+  }
+]);
