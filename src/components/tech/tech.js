6245aa1d45ab6da524523599e3203992fd46a72d
Vivek Chanddru <vivek.chanddru@gmail.com>
2025-01-28 20:35:57 -0800

    fix lint

--- src/components/tech/tech.js
+++ src/components/tech/tech.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
 import Section from '../common/section';
 import { techInfo } from '../../helpers/information';
